501a7176775c6e56a5fa5ec521a0266f717b61dc
ChrisThompsonDev <ChrisThompsonDev@protonmail.com>
2022-09-10 10:21:21 -0400

    added more client side javascript to prevent page reloads when adding or removing an item from the user's collection

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,64 @@
-const todoItem = document.querySelectorAll('div.false')
-const todoComplete = document.querySelectorAll('div.true')
+/* const todoItem = document.querySelectorAll('div.false')
+const todoComplete = document.querySelectorAll('div.true') */
 
+// change to 1 function: ChangeDivStatus
+// if statement: If false, run code for addDiv
+  //if true, run code for removeDivId
 
-Array.from(todoItem).forEach((el)=>{
+const taskItems = document.querySelectorAll('div')
+
+Array.from(taskItems).forEach((el)=>{
+  el.addEventListener('click', ChangeDivStatus)
+})
+
+async function ChangeDivStatus(){
+  if (this.getAttribute('class') === 'false'){
+    this.setAttribute("class", "true")
+    const userId = document.querySelector('h3[data-user-id]').dataset.userId
+    const divId = this.id
+    try {
+      await fetch('todos/addDivId', {
+        method: 'post',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+          userId: userId,
+          divId: divId
+        })
+      })
+      /* location.reload() */
+    } catch(err) {
+      console.log(err)
+    }
+  } else if (this.getAttribute('class') === 'true') {
+    console.log(`${this.id} to be removed`)
+    this.setAttribute("class", "false")
+    const userId = document.querySelector('h3[data-user-id]').dataset.userId
+    const divId = this.id
+    try {
+      await fetch('todos/removeDivId', {
+        method: 'post',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+          userId: userId,
+          divId: divId
+        })
+      })
+      /* location.reload() */
+    } catch(err) {
+      console.log(err)
+    }
+    }
+}
+
+/* Array.from(todoItem).forEach((el)=>{
     el.addEventListener('click', addDivId)
 })
 
 Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', removeDivId)
-})
+}) */
 
-async function addDivId(){
+/* async function addDivId(){
   this.setAttribute("class", "true")
   const userId = document.querySelector('h3[data-user-id]').dataset.userId
   const divId = this.id
@@ -23,7 +71,7 @@ async function addDivId(){
         divId: divId
       })
     })
-    /* location.reload() */
+    location.reload()
   } catch(err) {
     console.log(err)
   }
@@ -31,6 +79,7 @@ async function addDivId(){
 
 async function removeDivId(){
   console.log(`${this.id} to be removed`)
+  this.setAttribute("class", "false")
   const userId = document.querySelector('h3[data-user-id]').dataset.userId
   const divId = this.id
   try {
@@ -46,7 +95,7 @@ async function removeDivId(){
   } catch(err) {
     console.log(err)
   }
-}
+} */
 
 // *************** Store Last Tab so preserve tab selection on fage reload ************
 
